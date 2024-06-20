d60c94a1698644498c521d1b7b12e673512a221c
Renz Aguirre <renzaguirre.w@gmail.com>
2024-06-20 09:16:01 +0800

    Remove the start() function in calculator and put it in Main. Added calculate function in the Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -99,72 +99,15 @@ public class Calculator {
         return total;
     }
 
-    public void start(){
-
-        System.out.println("-----Calculator-----");
-
-        // Main Loop
-        boolean in_calculator = true;
-        while(in_calculator){
-
-            // Menu
-            System.out.println("1. Start");
-            System.out.println("2. Examples/Help");
-            System.out.println("3. Exit");
-
-            // Get the user Input
-            System.out.print("> ");
-            Scanner my_scanner = new Scanner(System.in);
-            String user_input = my_scanner.nextLine();
-
-            switch(user_input){
-
-                // Start the Calculator
-                case "1": {
-
-                    // Try to parse the user input
-                    Checker checker = new Checker();
-                    Parser parser = new Parser(checker);
-                    try {
-                        // Get User Input
-                        System.out.println("---Input");
-                        System.out.print(">");
-                        user_input = my_scanner.nextLine();
-                        parser.parseString(user_input);
-
-                        // Calculate the expression from the tree
-                        double value = calculate_from_tree(parser.getTree());
-
-                        // Print it
-                        System.out.println("The total is: " + value);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                }
-
-                // Print some examples
-                case "2":{
-                    System.out.println("\n---------------------------");
-                    System.out.println("Examples:");
-                    System.out.println("1.) 1+2+3*4/6*2");
-                    System.out.println("2.) 1 + 2 + 3 * 4 / 6 * 2");
-                    System.out.println("Notes:");
-                    System.out.println("* Doesn't support Parenthesis");
-                    System.out.println("---------------------------\n");
-                    break;
-                }
-
-                // Break the loop
-                case "3":{
-                    in_calculator = false;
-                    break;
-                }
-                default:{
-                    System.out.println("[!] Invalid Input");
-                    break;
-                }
-            }
+    public double calculate(String expression){
+        // Try to parse the user input
+        Checker checker = new Checker();
+        Parser parser = new Parser(checker);
+        try {
+            parser.parseString(expression);
+            return calculate_from_tree(parser.getTree());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
